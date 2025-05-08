7effdba3f6a31ae96e9134a1e6072e9406ffa75f
Hadi Ali Naqvi <hadi20143110@gmail.com>
2025-05-08 18:11:25 +0200

    Add magic_function examples

---
+++ ADEF.py
@@ -0,0 +1,16 @@
+def magic_function(Number, Numbert):
+    print(Number - Numbert)
+
+magic_function(12, 10)
+
+def magic_function(Number, Numbeto):
+    print(Number + Numbeto)
+
+magic_function(12, 10)
+
+def magic_function(Number, Numbto):
+    print(Number / Numbto)
+
+magic_function(12, 10)
+
+
