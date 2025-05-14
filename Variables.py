bcc2b4ab09f70556a437f4b92eaf4c10c162bb82
PythonDebugger <hadi20143110@gmail.com>
2025-05-14 20:58:14 +0200

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,19 @@
+#rules of defining variables
+#1) variables cannot start with a digit/number
+#1a = 50
+#print (1a)       this is wrong way
+
+aa = 20
+print(aa)
+
+#2) variables are case sensitive
+a = 200
+#print(A)                  wrong
+print(a)
+#3) variables must start with alphabet or underscore_
+b = 200
+print(b)
+_b = 200
+print (_b)
+B = 1000
+print(B)
---
+++ import tkinter as tk.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+root = tk.Tk()
+tk.Label(root, text="Ronaldo").pack()
+root.mainloop()
